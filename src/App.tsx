86ced4d511102a6b3047a4500d2aa36f37f0c51e
Richard <richardpires663802@gmail.com>
2023-07-07 13:32:11 -0300

    add: files and folders

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { GlobalStyles } from "./styles/global";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <h2>Estou aqui</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
